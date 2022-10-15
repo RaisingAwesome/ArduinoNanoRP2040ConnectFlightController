3542bf93bfbb1a12ca832db2593eb543bbdb8f29
Sean Miller <38836869+RaisingAwesome@users.noreply.github.com>
2022-10-15 13:35:21 -0500

    Removed unnecessary code

--- Rawpter.ino
+++ Rawpter.ino
@@ -40,12 +40,11 @@ float maxYaw = 160.0;     //Max yaw rate in deg/sec (default 160.0)
 
 float Kp_roll_angle = 0.2;    //Roll P-gain - angle mode 
 float Ki_roll_angle = 0.3;    //Roll I-gain - angle mode
-float Kd_roll_angle = 0.05;   //Roll D-gain - angle mode (has no effect on controlANGLE2)
-float B_loop_roll = 0.9;      //Roll damping term for controlANGLE2(), lower is more damping (must be between 0 to 1, originally was 0.9)
+float Kd_roll_angle = 0.05;   //Roll D-gain - angle mode
+
 float Kp_pitch_angle = 0.2;   //Pitch P-gain - angle mode
 float Ki_pitch_angle = 0.3;   //Pitch I-gain - angle mode
-float Kd_pitch_angle = 0.05;  //Pitch D-gain - angle mode (has no effect on controlANGLE2)
-float B_loop_pitch = 0.9;     //Pitch damping term for controlANGLE2(), lower is more damping (must be between 0 to 1)
+float Kd_pitch_angle = 0.05;  //Pitch D-gain - angle mode
 
 float Kp_roll_rate = 0.15;    //Roll P-gain - rate mode
 float Ki_roll_rate = 0.2;     //Roll I-gain - rate mode
@@ -759,139 +758,6 @@ void controlANGLE() {
   integral_roll_prev = integral_roll;
   //Update pitch variables
   integral_pitch_prev = integral_pitch;
-  //Update yaw variables
-  error_yaw_prev = error_yaw;
-  integral_yaw_prev = integral_yaw;
-}
-
-void controlANGLE2() {
-  //DESCRIPTION: Computes control commands based on state error (angle) in cascaded scheme
-  /*
-   * Gives better performance than controlANGLE() but requires much more tuning. Not reccommended for first-time setup.
-   * See the documentation for tuning this controller.
-   */
-  //Outer loop - PID on angle
-  float roll_des_ol, pitch_des_ol;
-  //Roll
-  error_roll = roll_des - roll_IMU;
-  integral_roll_ol = integral_roll_prev_ol + error_roll*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_roll_ol = 0;
-  }
-  integral_roll_ol = constrain(integral_roll_ol, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_roll = (roll_IMU - roll_IMU_prev)/dt1; 
-  roll_des_ol = Kp_roll_angle*error_roll + Ki_roll_angle*integral_roll_ol;// - Kd_roll_angle*derivative_roll;
-
-  //Pitch
-  error_pitch = pitch_des - pitch_IMU;
-  integral_pitch_ol = integral_pitch_prev_ol + error_pitch*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_pitch_ol = 0;
-  }
-  integral_pitch_ol = constrain(integral_pitch_ol, -i_limit, i_limit); //saturate integrator to prevent unsafe buildup
-  derivative_pitch = (pitch_IMU - pitch_IMU_prev)/dt1;
-  pitch_des_ol = Kp_pitch_angle*error_pitch + Ki_pitch_angle*integral_pitch_ol;// - Kd_pitch_angle*derivative_pitch;
-
-  //Apply loop gain, constrain, and LP filter for artificial damping
-  float Kl = 30.0;
-  roll_des_ol = Kl*roll_des_ol;
-  pitch_des_ol = Kl*pitch_des_ol;
-  roll_des_ol = constrain(roll_des_ol, -240.0, 240.0);
-  pitch_des_ol = constrain(pitch_des_ol, -240.0, 240.0);
-  roll_des_ol = (1.0 - B_loop_roll)*roll_des_prev + B_loop_roll*roll_des_ol;
-  pitch_des_ol = (1.0 - B_loop_pitch)*pitch_des_prev + B_loop_pitch*pitch_des_ol;
-
-  //Inner loop - PID on rate
-  //Roll
-  error_roll = roll_des_ol - GyroX;
-  integral_roll_il = integral_roll_prev_il + error_roll*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_roll_il = 0;
-  }
-  integral_roll_il = constrain(integral_roll_il, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_roll = (error_roll - error_roll_prev)/dt1; 
-  roll_PID = .01*(Kp_roll_rate*error_roll + Ki_roll_rate*integral_roll_il + Kd_roll_rate*derivative_roll); //Scaled by .01 to bring within -1 to 1 range
-
-  //Pitch
-  error_pitch = pitch_des_ol - GyroY;
-  integral_pitch_il = integral_pitch_prev_il + error_pitch*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_pitch_il = 0;
-  }
-  integral_pitch_il = constrain(integral_pitch_il, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_pitch = (error_pitch - error_pitch_prev)/dt1; 
-  pitch_PID = .01*(Kp_pitch_rate*error_pitch + Ki_pitch_rate*integral_pitch_il + Kd_pitch_rate*derivative_pitch); //Scaled by .01 to bring within -1 to 1 range
-  
-  //Yaw
-  error_yaw = yaw_des - GyroZ;
-  integral_yaw = integral_yaw_prev + error_yaw*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_yaw = 0;
-  }
-  integral_yaw = constrain(integral_yaw, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_yaw = (error_yaw - error_yaw_prev)/dt1; 
-  yaw_PID = .01*(Kp_yaw*error_yaw + Ki_yaw*integral_yaw + Kd_yaw*derivative_yaw); //Scaled by .01 to bring within -1 to 1 range
-  
-  //Update roll variables
-  integral_roll_prev_ol = integral_roll_ol;
-  integral_roll_prev_il = integral_roll_il;
-  error_roll_prev = error_roll;
-  roll_IMU_prev = roll_IMU;
-  roll_des_prev = roll_des_ol;
-  //Update pitch variables
-  integral_pitch_prev_ol = integral_pitch_ol;
-  integral_pitch_prev_il = integral_pitch_il;
-  error_pitch_prev = error_pitch;
-  pitch_IMU_prev = pitch_IMU;
-  pitch_des_prev = pitch_des_ol;
-  //Update yaw variables
-  error_yaw_prev = error_yaw;
-  integral_yaw_prev = integral_yaw;
-}
-
-void controlRATE() {
-  //DESCRIPTION: Computes control commands based on state error (rate)
-  /*
-   * See explanation for controlANGLE(). Everything is the same here except the error is now the desired rate - raw gyro reading.
-   */
-  //Roll
-  error_roll = roll_des - GyroX;
-  integral_roll = integral_roll_prev + error_roll*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_roll = 0;
-  }
-  integral_roll = constrain(integral_roll, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_roll = (error_roll - error_roll_prev)/dt1; 
-  roll_PID = .01*(Kp_roll_rate*error_roll + Ki_roll_rate*integral_roll + Kd_roll_rate*derivative_roll); //Scaled by .01 to bring within -1 to 1 range
-
-  //Pitch
-  error_pitch = pitch_des - GyroY;
-  integral_pitch = integral_pitch_prev + error_pitch*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_pitch = 0;
-  }
-  integral_pitch = constrain(integral_pitch, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_pitch = (error_pitch - error_pitch_prev)/dt1; 
-  pitch_PID = .01*(Kp_pitch_rate*error_pitch + Ki_pitch_rate*integral_pitch + Kd_pitch_rate*derivative_pitch); //Scaled by .01 to bring within -1 to 1 range
-
-  //Yaw, stablize on rate from GyroZ
-  error_yaw = yaw_des - GyroZ;
-  integral_yaw = integral_yaw_prev + error_yaw*dt1;
-  if (PWM_throttle < 1060) {   //Don't let integrator build if throttle is too low
-    integral_yaw = 0;
-  }
-  integral_yaw = constrain(integral_yaw, -i_limit, i_limit); //Saturate integrator to prevent unsafe buildup
-  derivative_yaw = (error_yaw - error_yaw_prev)/dt1; 
-  yaw_PID = .01*(Kp_yaw*error_yaw + Ki_yaw*integral_yaw + Kd_yaw*derivative_yaw); //Scaled by .01 to bring within -1 to 1 range
-
-  //Update roll variables
-  error_roll_prev = error_roll;
-  integral_roll_prev = integral_roll;
-  GyroX_prev = GyroX;
-  //Update pitch variables
-  error_pitch_prev = error_pitch;
-  integral_pitch_prev = integral_pitch;
-  GyroY_prev = GyroY;
   //Update yaw variables
   error_yaw_prev = error_yaw;
   integral_yaw_prev = integral_yaw;
